675395763ef9f7e6ce21970db4088ee6da8cfafe
Michael Ladderbush <38595703+mikeladderbush@users.noreply.github.com>
2023-10-26 18:06:34 -0400

    Got dual stat graphing working

--- data_analysis_functions.py
+++ data_analysis_functions.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def create_graph(input):
+def create_graph(*args):
     week = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17])
-    stat = np.array(input)
-    print(stat)
     
-    plt.plot(week,stat,label="Chosen Stat Per Game")
+    for item in args:
+        print(item)
+        for stat in item:
+            print(stat)
+            plt.plot(week,stat)
     
     plt.show()
--- data_fetch_functions.py
+++ data_fetch_functions.py
@@ -4,22 +4,26 @@ import data_analysis_functions
 from bs4 import BeautifulSoup
             
 def fetch_chosen_stats(selected_url, stat_vector):
-    x = []
+    chosen_stat_vector = []
+    vector_of_stat_vectors = []
     if selected_url:
         response = requests.get(selected_url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
             for item in stat_vector:
+                print(item)
                 chosen_stat = soup.find_all(attrs={'class': 'right', 'data-stat': item})
                 if chosen_stat:
                     for stat in chosen_stat:
                         if stat.get_text():
-                            x.append(int(stat.get_text()))
+                            chosen_stat_vector.append(int(stat.get_text()))
                         else:
-                            x.append(int(0))
+                            chosen_stat_vector.append(int(0))
                 else:
                     print('Data not found.\n')
-                data_analysis_functions.create_graph(x)
+                vector_of_stat_vectors.append(chosen_stat_vector)
+                chosen_stat_vector = []
+            data_analysis_functions.create_graph(vector_of_stat_vectors)
         else:
             print('Failed to retrive the web page.')
             
