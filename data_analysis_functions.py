45962c982e68824d80028b02a85672a8a28d5e08
Michael Ladderbush <38595703+mikeladderbush@users.noreply.github.com>
2023-10-27 10:46:02 -0400

    Set up labels on x-axis

--- data_analysis_functions.py
+++ data_analysis_functions.py
@@ -2,12 +2,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def create_graph(*args):
+    input_vector = []
+    input_numpy_arrays = []
+    opponent_array = args[0].pop()
     week = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17])
     
-    for item in args:
-        print(item)
-        for stat in item:
-            print(stat)
-            plt.plot(week,stat)
+    ax = plt.axes()
+    ax.set_xticks(week)
+    ax.set_xticklabels(opponent_array)
     
+    for vector in args:
+        input_vector.append(vector)
+    for vector in input_vector:
+        input_numpy_arrays.append(np.array(vector))
+    for item in input_numpy_arrays:
+        for stat in item:
+            plt.plot(week,stat)
     plt.show()
--- data_fetch_functions.py
+++ data_fetch_functions.py
@@ -4,25 +4,31 @@ import data_analysis_functions
 from bs4 import BeautifulSoup
             
 def fetch_chosen_stats(selected_url, stat_vector):
+    opponent_vector = []
     chosen_stat_vector = []
     vector_of_stat_vectors = []
     if selected_url:
         response = requests.get(selected_url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
+            opponent = soup.find_all(attrs={'class': 'left', 'data-stat': 'opp'})
+            if opponent:
+                for team in opponent:
+                    if team.get_text():
+                        opponent_vector.append(team.get_text())
             for item in stat_vector:
-                print(item)
                 chosen_stat = soup.find_all(attrs={'class': 'right', 'data-stat': item})
                 if chosen_stat:
                     for stat in chosen_stat:
                         if stat.get_text():
                             chosen_stat_vector.append(int(stat.get_text()))
                         else:
-                            chosen_stat_vector.append(int(0))
+                            chosen_stat_vector.append(int(0))                  
                 else:
                     print('Data not found.\n')
                 vector_of_stat_vectors.append(chosen_stat_vector)
                 chosen_stat_vector = []
+            vector_of_stat_vectors.append(opponent_vector)
             data_analysis_functions.create_graph(vector_of_stat_vectors)
         else:
             print('Failed to retrive the web page.')
